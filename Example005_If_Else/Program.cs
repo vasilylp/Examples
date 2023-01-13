508d40a8ae306d8beeda661f4d72147e0efa71d9
Vpolyushkevich@gmail.com <Vasily>
2023-01-13 13:41:28 +0300

    Add If - Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя : ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура! Это же Маща!");
+}
+else
+{
+    Console.Write("Привет! ");
+    Console.WriteLine(username);
+}
