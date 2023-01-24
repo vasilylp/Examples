5e70cc8686881032492ac03d2aad5fc9106318ff
Vpolyushkevich@gmail.com <Vasily>
2023-01-24 21:58:52 +0300

    Add Seminsr4

---
+++ Example017_seminar4.1/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int Sum(int number)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+}
+
+int InputNumber(string str)
+{
+    int number;
+    string text;
+    while (true)
+    {
+        Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+
+        System.Console.WriteLine("Введено не корректное число, попробуйте ещё раз!");
+    }
+    return number;
+}
+
+int InputNumberEugene(string message)
+{
+    int number;
+    while (true)
+    {
+        System.Console.Write(message);
+        if (!(int.TryParse(Console.ReadLine(), out number)))
+        {
+            System.Console.WriteLine("Введено некорректное число, попробуйте еще раз.");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return number;
+}
+
+int A = InputNumber("Введите число А: ");
+int B = InputNumberEugene("Введите число В: ");
+
+int sumA = Sum(A);
+int sumB = Sum(B);
+
+System.Console.WriteLine($"Сумма чисел от 1 до {A}: {sumA}");
+System.Console.WriteLine($"Сумма чисел от 1 до {B}: {sumB}");
+
---
+++ Example018_Seminar4.1.1/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 
+//_______________________________________________________________________________________
+
+// System.Console.WriteLine("Введите пятизначное число :");
+// string? numb = Console.ReadLine();
+// int lenghtNum = numb!.Length;
+// int i = 0;
+// while(i < lenghtNum)
+// {
+//     i++;
+// }
+// System.Console.WriteLine($"{numb} -> {i}");
+// Решение 2___________________________________________________________________
+
+
+// int InputNumber(string str)
+// {
+//     int number;
+//     string text;
+//     while (true)
+//     {
+//         Console.Write(str);
+//         text = Console.ReadLine();
+//         if (int.TryParse(text, out number))
+//         {
+//             break;
+//         }
+//         System.Console.WriteLine("Введено не корректное число");
+//     }
+//     return number;
+
+// }
+
+// void Counter(string number)
+// {
+//     int answer = 0;
+//     for (int i = 0; i < number.Length; i++)
+//     {
+//         answer++;
+//     }
+//     System.Console.WriteLine(answer);
+// }
+
+// int numb = InputNumber("Введите число: ");
+// Counter(Convert.ToString(numb));
+
+//___________________________________________________________________________
+
+// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 12
+//_______________________________________________________________________________________________
+int InputNumber(string str)
+{
+    int number;
+    string? text;
+    while (true)
+    {
+        Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Введено не число");
+    }
+    return number;
+}
+
+void Factorial(int count)
+{
+    int answer = 1;
+    for (int i = 1; i < count + 1; i++)
+    {
+        answer *= i;
+    }
+    System.Console.WriteLine(answer);
+}
+
+int N = InputNumber("Введите число N:");
+Factorial(N);
