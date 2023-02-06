27523696161b794a47ce3ace383300f6ab7ff475
Vpolyushkevich@gmail.com <Vasily>
2023-02-06 11:13:00 +0300

    Add Seminar 006 and 007

---
+++ Seminar006/Program.cs
@@ -0,0 +1,183 @@
+﻿//------------------------Семинар 6---------------------------------------
+
+// int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+// {
+//     int[] arr = new int[size];
+//     Random rand = new Random();
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = rand.Next(leftRange, rightRange + 1);
+//     }
+//     return arr;
+// }
+
+// void PrintArray(int[] arr)
+// {
+//     System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+// }
+
+// void ReversArray(int[]arr)
+// {
+//               int temp;
+//               for (int i = 0; i < arr.Length /2 ; i++)
+//               {
+//                     temp = arr[i];
+//                     arr[i] = arr[arr.Length-i-1] ;
+//                     arr[arr.Length-i-1] =arr[i]  ;     
+//               }
+//               return arr;
+// }
+
+
+// int[] array = FillArrayWithRandomNumbers(10, -10, 10);
+// PrintArray(array);
+
+
+///==============================================================================
+// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать 
+// треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+//---------------------------Решение---------------------------------------------------
+
+// int InputNumber(string str)
+// {
+//               int number;
+//               string? text;
+//               while (true)
+//               {
+//                             Console.Write(str);
+//                             text = Console.ReadLine();
+//                             if (int.TryParse(text, out number))
+//                             {
+//                                           break;
+//                             }
+
+//                             System.Console.WriteLine("Введено не корректное число, попробуйте ещё раз!");
+//               }
+//               return number;
+// }
+
+
+// bool TriangleCheck(int numb1, int numb2, int numb3)
+// {
+//               if ((numb1 + numb2) > numb3 && (numb1 + numb3) > numb2 && (numb3 + numb2) > numb1)
+//               {
+//                             return true;
+//               }
+
+//               return false;
+// }
+
+
+// int firstNum = InputNumber("Введите первое число :");
+// int secondNum = InputNumber("Введите второе число :");
+// int thirdNum = InputNumber("Введите третье число :");
+
+// if (TriangleCheck(firstNum, secondNum, thirdNum))
+// {
+//               System.Console.WriteLine("Да, такой треугольник существует");
+// }
+// else
+// {
+//               System.Console.WriteLine("Нет, такого треугольника не существует");
+// }
+
+//=============================Рекурсия=======================================================
+// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101InputNumber("Введите второе число :");
+// 3  -> 11
+// 2  -> 10
+//--------------------------------------Решение--------------------------------------------------
+// void GetBinaryView(int num)
+// {
+//               if (num <= 0) return;
+//               GetBinaryView(num / 2);
+//               System.Console.Write(num % 2);
+// }
+
+// System.Console.Write("Введите число :");
+// int n = Convert.ToInt32(Console.ReadLine());
+// GetBinaryView(n);
+//========================================================================================
+
+// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+//Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+//---------------------------------Решение-------------------------------------
+// int InputNumber(string str)
+// {
+//               int number;
+//               string? text;
+//               while (true)
+//               {
+//                             Console.Write(str);
+//                             text = Console.ReadLine();
+//                             if (int.TryParse(text, out number))
+//                             {
+//                                           break;
+//                             }
+
+//                             System.Console.WriteLine("Введено не корректное число, попробуйте ещё раз!");
+//               }
+//               return number;
+// }
+
+// void Fibonachi(int n)
+// {
+//               string[] fibb = new string[n];
+//               int fibbN = 0;
+//               int i = 0;
+//               while (i < n)
+//               {
+//                             if (i == 0) fibbN = 0;
+//                             else if (i == 1 || i == 2) fibbN = 1;
+//                             else if (i > 2) fibbN = Convert.ToInt32(Convert.ToString(fibb[i - 1])) + Convert.ToInt32(Convert.ToString(fibb[i - 2]));
+//                             fibb[i] = Convert.ToString(Convert.ToInt32(fibbN));
+//                             i++;
+//               }
+//               Console.WriteLine("[" + string.Join(",", fibb) + "]");
+
+// }
+
+
+
+// int n = InputNumber("Введите первое число : ");
+// Fibonachi(n);
+
+
+
+
+//============================================================================================
+// Задача 45: Напишите программу, которая будет создавать копию заданного массива
+// с помощью поэлементного копирования.
+//---------------------------Решение-----------------------------------------
+
+// int[] RandomArrayCreation(int size, int leftValue, int rightValue)
+// {
+//     int[] arr = new int[size];
+//     Random rand = new Random();
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = rand.Next(leftValue, rightValue + 1);
+//     }
+//     return arr;
+// }
+
+// int[] ArrayCopying(int[] array)
+// {
+//     int[] arrayCopy = new int[array.Length];
+//     for (int i = 0; i < arrayCopy.Length; i++)
+//     {
+//         arrayCopy[i] = array[i];
+//     }
+//     return arrayCopy;
+// }
+
+
+// int[] arr = RandomArrayCreation(5, 0, 10);
+// Console.WriteLine("[" + string.Join(",", arr) + "]");
+// int[] arr2 = ArrayCopying(arr);
+// Console.WriteLine("[" + string.Join(",", arr2) + "]");
+//=====================================================================================
---
+++ Seminar007/Program.cs
@@ -0,0 +1,296 @@
+﻿// // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// //-----------------------------Решение----------------------------------------
+
+// double InputNumber(string message)
+// {
+//               double number;
+//               while (true)
+//               {
+//                             System.Console.Write(message);
+//                             if (!(double.TryParse(Console.ReadLine(), out number)))
+//                             {
+//                                           System.Console.WriteLine("Введено некорректное число, попробуйте еще раз.");
+//                             }
+//                             else
+//                             {
+//                                           break;
+//                             }
+//               }
+//               return number;
+// }
+
+// void IntersectionPoint(double b1, double k1, double b2, double k2)
+// {
+//               double x = (b2 - b1) / (k1 - k2);
+//               double y = (k1 * x) + b1;
+//               System.Console.WriteLine($"b1 = {b1}, k1 = {k1},b2 = {b2}, k2 = {k2} ");
+//               System.Console.WriteLine($"Координаты пересечения двух прямых ({Math.Round(x, 2)}; {Math.Round(y, 2)})");
+// }
+
+
+// double b1 = InputNumber("Введите значение b1 : ");
+// double k1 = InputNumber("Введите значение k1 : ");
+// double b2 = InputNumber("Введите значение b2 : ");
+// double k2 = InputNumber("Введите значение k2 : ");
+// IntersectionPoint(b1, k1, b2, k2);
+//==============================================================================================================
+// // Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// //Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// // 0, 7, 8, -2, -2 -> 2
+// // 1, -7, 567, 89, 223-> 3
+// //----------------------Решение--------------------------------
+
+// int[] InputNumbers(string str, int m, string str1)
+// {
+//               int[] array = new int[m];
+//               int number;
+//               string? text;
+//               for (int i = 0; i < m; i++)
+//               {
+//                             Console.Write($"{str} {m} {str1}, {i + 1}-е число :");
+//                             text = Console.ReadLine();
+//                             if (int.TryParse(text, out number))
+//                             {
+//                                           array[i] = number;
+
+//                             }
+//                             else
+//                             {
+//                                           System.Console.WriteLine("Введено не корректное число, попробуйте ещё раз!");
+//                                           i = i - 1;
+//                             }
+//               }
+//               return array;
+// }
+
+// void PrintArray(int[] arr)
+// {
+//               System.Console.WriteLine("[ " + String.Join(", ", arr) + " ]");
+// }
+
+
+
+// void MoreZero(int[] array)
+// {
+//               int numMoreZero = 0;
+//               for (int i = 0; i < array.Length; i++)
+//               {
+//                             if (array[i] > 0)
+//                             {
+//                                           numMoreZero++;
+//                             }
+//               }
+//               System.Console.WriteLine($"Чисел больше 0 : {numMoreZero}");
+// }
+
+
+// int[] arr = InputNumbers("Введите", 6, "чисел");
+// PrintArray(arr);
+// MoreZero(arr);
+//==================================== Семинар 7====================================
+
+// // Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// // m = 3, n = 4.
+// // 1 4 8 19
+// // 5 -2 33 -2
+// // 77 3 8 1
+// int[,] FillMatrix(int rows, int cols)
+// {
+//     Random rand = new Random();
+//     int[,] matr = new int[rows, cols];
+
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < cols; j++)
+//         {
+//             matr[i, j] = rand.Next(0, 100);
+//         }
+//     }
+
+//     return matr;
+// }
+
+// void PrintMatrix(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             System.Console.Write(matr[i, j] + "\t");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// System.Console.WriteLine("Введите m и n: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = FillMatrix(m, n);
+// System.Console.WriteLine();
+// PrintMatrix(matrix);
+//=====================================================================================
+// // Задача 48: Задайте двумерный массив размера m на n, каждый элемент 
+// // в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// // rows = 3, cols = 4.
+// // 0 1 2 3
+// // 1 2 3 4
+// // 2 3 4 5
+// //-------------------------------Решение-------------------------------------
+// int[,] FillMatrix(int rows, int cols)
+// {
+//     int[,] matr = new int[rows, cols];
+
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < cols; j++)
+//         {
+//             matr[i, j] = i + j;
+//         }
+//     }
+
+//     return matr;
+// }
+
+// void PrintMatrix(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             System.Console.Write(matr[i, j] + "\t");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// System.Console.WriteLine("Введите m и n: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = FillMatrix(m, n);
+// System.Console.WriteLine();
+// PrintMatrix(matrix);
+//====================================================================
+
+// Решение в группах задач:
+// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса нечётные,
+// и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+//-----------------------------------------------------------------------
+
+// // Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса нечётные, 
+// // и замените эти элементы на их квадраты
+
+// int[,] FillMatrix(int rows, int cols)
+// {
+//     int[,] matr = new int[rows, cols];
+//     Random rand = new Random();
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < cols; j++)
+//         {
+//             matr[i, j] = rand.Next(1, 10);
+//         }
+//     }
+//     return matr;
+// }
+
+// void PrintMatrix(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             System.Console.Write(matr[i, j] + "\t");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// void MatrixEdit(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if ((i % 2 != 0) && (j % 2 != 0))
+//             {
+//                 matrix[i, j] = (int)Math.Pow(matrix[i, j], 2);
+//             }
+//         }
+
+//     }
+// }
+
+// int[,] matrix = FillMatrix(3, 3);
+// PrintMatrix(matrix);
+// System.Console.WriteLine();
+// MatrixEdit(matrix);
+// PrintMatrix(matrix);
+//================================================================================
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+//------------------------------------------------------------------------------
+
+int[,] FillMatrix(int rows, int cols)
+{
+    int[,] matr = new int[rows, cols];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matr[i, j] = rand.Next(1, 10);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int MatrixEdit(int[,] matrix)
+{
+    int result = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if ((i == j))
+            {
+                result += matrix[i, j];
+            }
+        }
+    }
+    return result;
+}
+
+
+System.Console.WriteLine("Введите rows и cols: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+int cols = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+int[,] array2d = FillMatrix(rows, cols);
+PrintMatrix(array2d);
+System.Console.WriteLine();
+System.Console.WriteLine(MatrixEdit(array2d));
